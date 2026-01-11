9909c41e62a7f6e8311f715ae38a22deacb55022
ggjjc786-boop <ggjjc786@gmail.com>
2026-01-11 13:49:35 +0800

    Site updated: 2026-01-11 13:49:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/13/easylink-la-vps-review/","2026/01/10/hello-world/","2024/08/15/69yun-airport/"];function toRandomPost(){
+var posts=[];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
