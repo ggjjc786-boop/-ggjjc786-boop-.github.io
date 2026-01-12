f49bbb2aa51ee892ad407cb2b1a68a33621a0201
ggjjc786-boop <ggjjc786@gmail.com>
2026-01-12 10:26:25 +0800

    fix_worker

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,24 +1,5 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
-
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event));
-});
-
-async function handleRequest(event) {
-  try {
-    return await getAssetFromKV(event);
-  } catch (e) {
-    // 404 处理
-    try {
-      let notFoundResponse = await getAssetFromKV(event, {
-        mapRequestToAsset: (req) => new Request(`${new URL(req.url).origin}/404.html`, req),
-      });
-      return new Response(notFoundResponse.body, {
-        ...notFoundResponse,
-        status: 404,
-      });
-    } catch (e) {
-      return new Response('Not Found', { status: 404 });
-    }
+export default {
+  async fetch(request, env) {
+    return env.ASSETS.fetch(request);
   }
-}
+};
