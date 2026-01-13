5ab1b2975191d9e86df3f268ea4b224c6a466ee2
ggjjc786-boop <ggjjc786@gmail.com>
2026-01-13 10:24:13 +0800

    Site updated: 2026-01-13 10:24:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/11/idm-activation-tool/"];function toRandomPost(){
+var posts=["2026/01/11/idm-activation-tool/","2026/01/10/md-practice/"];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
